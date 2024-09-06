1554d3718429b1edc9d5dbfbafa2d5d23a1ff70e
Durso <dursoarthur@gmail.com>
2024-09-05 23:45:59 -0300

    Adicionando Desafio e Finalizando Exercicios

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-transacao = {'valor': 12000, 'hora': 20}
-
-if (9 > transacao["hora"] < 18):
-    print("Transacao suspeita.")
-else:
-    print("a")
-
-
 ### Exercício 1: Verificação de Qualidade de Dados
 # Você está analisando um conjunto de dados de vendas e precisa garantir 
 # que todos os registros tenham valores positivos para `quantidade` e `preço`. 
@@ -52,6 +44,7 @@ if log["level"] == 'ERROR':
 # você precisa garantir que cada usuário tenha idade entre 18 e 65 anos e tenha 
 # fornecido um email válido. Escreva um programa que valide essas condições 
 # e imprima "Dados de usuário válidos" ou o erro específico encontrado.
+
 import re
 
 idade = int(input("Qual a sua idade? "))
@@ -78,37 +71,3 @@ if transacao["valor"] > 10000 and (9 < transacao["hora"] < 18):
     print("Transacao suspeita.")
 else:
     print("Transacao nao suspeita.")
-
-### Exercício 6. Contagem de Palavras em Textos
-# Objetivo:** Dado um texto, contar quantas vezes cada palavra única aparece nele.
-
-
-
-### Exercício 7. Normalização de Dados
-# Objetivo:** Normalizar uma lista de números para que fiquem na escala de 0 a 1.
-
-### Exercício 8. Filtragem de Dados Faltantes
-# Objetivo:** Dada uma lista de dicionários representando dados de usuários, filtrar aqueles que têm um campo específico faltando
-
-### Exercício 9. Extração de Subconjuntos de Dados
-# Objetivo:** Dada uma lista de números, extrair apenas aqueles que são pares.
-
-### Exercício 10. Agregação de Dados por Categoria
-# Objetivo:** Dado um conjunto de registros de vendas, calcular o total de vendas por categoria.
-
-### Exercícios com WHILE
-
-### Exercício 11. Leitura de Dados até Flag
-# Ler dados de entrada até que uma palavra-chave específica ("sair") seja fornecida.
-
-### Exercício 12. Validação de Entrada
-# Solicitar ao usuário um número dentro de um intervalo específico até que a entrada seja válida.
-
-### Exercício 13. Consumo de API Simulado
-# Simular o consumo de uma API paginada, onde cada "página" de dados é processada em loop até que não haja mais páginas.
-
-### Exercício 14. Tentativas de Conexão
-# Simular tentativas de reconexão a um serviço com um limite máximo de tentativas.
-
-### Exercício 15. Processamento de Dados com Condição de Parada
-# Processar itens de uma lista até encontrar um valor específico que indica a parada.
